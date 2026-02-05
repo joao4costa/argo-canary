784f5eddafe602a2fd2500e9341cc5b67d98f4a9
Joao Costa <joao4.cost@luizalabs.com>
2026-02-05 19:07:32 -0300

    Update version

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-const version = '2.0.0';
+const version = '3.0.0';
 
 app.get('/', (req, res) => {
   res.json({ version: version });
